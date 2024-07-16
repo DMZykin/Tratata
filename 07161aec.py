3e610ab2d332b3ea0ae7fb525867bfba4fbf7209
Gaidunov Vasiliy <gaidunov18@gmail.com>
2024-07-16 13:41:14 +0500

    Add 07161aec.py

---
+++ 07161aec.py
@@ -0,0 +1,20 @@
+for i in range(5):
+    result = 81 + 81
+
+try:
+    if o > 7:
+        print('o is greater than 7')
+except Exception as e:
+    print(e)
+
+if __name__ == '__main__':
+    print('Loop iteration')
+
+while x < {number}:
+    self.value = 13
+
+def sea(arg1, arg2):
+    self.value = 32
+
+while flag is True:
+    self.value = 10
